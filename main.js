c2b01445ef25d30e4c703bdf24f540f877de3b2c
miguel <162368560+calangoxt@users.noreply.github.com>
2024-04-29 17:14:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+print('Hello World');
+var numero1 = 5;
+var numero2 = 8;
+var resultado;
+resultado= numero1 === numero2;
+print(numero1,'=',numero2,'?',resultado)
+resultado= numero1 !== numero2;
+print(numero1,'!=',numero2,'?',resultado)
+resultado= numero1 > numero2;
+print(numero1,'>',numero2,'?',resultado)
+resultado= numero1 < numero2;
+print(numero1,'<',numero2,'?',resultado)
+resultado= numero1 >= numero2;
+print(numero1,'>=',numero2,'?',resultado)
+resultado= numero1 <= numero2;
+print(numero1,'<=',numero2,'?',resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
